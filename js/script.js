d00e39e5642aa4840518ea5e3aa5f5749155f5c3
nino <nino@isideweb.com>
2024-05-16 11:15:56 +0200

    Divisione in cartelle

--- js/script.js
+++ js/script.js
@@ -43,7 +43,7 @@ function countdown(targetTime, targetDivId) {
 
 window.onload = function() {
     // Effettua una richiesta HTTP per recuperare le impostazioni dall'esterno
-    fetch('settings.json')
+    fetch('js/settings.json')
         .then(response => response.json())
         .then(settings => {
             // Crea una nuova data basata sulle impostazioni ottenute dal file JSON
