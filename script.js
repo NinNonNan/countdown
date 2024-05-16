84008e97d8708a8e585a21f5a830ddb92d1fbd7a
nino <nino@isideweb.com>
2024-05-16 11:36:25 +0200

    correzione

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function countdown(targetTime, targetDivId) {
+function countdown(targetTime, targetDivId, title, msg) {
     var targetDiv = document.getElementById(targetDivId);
     
     // Creazione di elementi per la stringa di testo e per l'ora del conto alla rovescia
@@ -18,7 +18,7 @@ function countdown(targetTime, targetDivId) {
         var difference = targetTime.getTime() - currentTime.getTime();
 
         if (difference <= 0) {
-            timeElement.innerHTML = "Tempo scaduto";
+            timeElement.innerHTML = msg;
             clearInterval(interval);
             return;
         }
@@ -34,12 +34,14 @@ function countdown(targetTime, targetDivId) {
         timeElement.innerHTML = formattedTime;
 
         // Aggiorna il titolo della pagina con l'ora del conto alla rovescia
-        document.title = "La live comincia tra: " + formattedTime;
+        document.title = title + " " + formattedTime;
     }
 
     var interval = setInterval(updateCountdown, 1000);
     updateCountdown();
 }
+
+
 
 window.onload = function() {
     // Effettua una richiesta HTTP per recuperare le impostazioni dall'esterno
@@ -48,13 +50,10 @@ window.onload = function() {
         .then(settings => {
             // Crea una nuova data basata sulle impostazioni ottenute dal file JSON
             var targetTime = new Date(settings.year, settings.month - 1, settings.day, settings.hour, settings.minute, settings.second);
-            // Chiama la funzione countdown con l'orario di destinazione e l'ID del div
-            countdown(targetTime, 'countdownDiv');
-
-            // Imposta il titolo della pagina e il messaggio di scadenza basati sulle variabili ottenute dal file JSON
-            document.title = settings.title;
-            timeElement.innerHTML = settings.msg;
+            // Chiama la funzione countdown con l'orario di destinazione, l'ID del div, il titolo e il messaggio
+            countdown(targetTime, 'countdownDiv', settings.title, settings.msg);
         })
         .catch(error => console.error('Errore nel recupero delle impostazioni:', error));
 };
+
 
